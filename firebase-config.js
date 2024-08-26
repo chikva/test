75818ea6cd8dcc853fbb7625579ad11705e31fa8
chikva <105802005+chikva@users.noreply.github.com>
2024-08-26 22:52:20 +0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 // Replace these with your Firebase project configuration values
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "YOUR_DATABASE_URL",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+     apiKey: "AIzaSyBp7oS3gUpMzPOhyD4sAifXc2W65EqMoiw",
+  authDomain: "mission-6cb34.firebaseapp.com",
+  databaseURL: "https://mission-6cb34-default-rtdb.firebaseio.com",
+  projectId: "mission-6cb34",
+  storageBucket: "mission-6cb34.appspot.com",
+  messagingSenderId: "737331310215",
+  appId: "1:737331310215:web:61a99ae636095469c49222",
+  measurementId: "G-9ZQ9NF8X8G"
 };
